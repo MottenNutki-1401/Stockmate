b7608d494a31c077d5eb1d567136897002cb4cd9
Gail Parazo <202310763@gordoncollege.edu.ph>
2024-12-09 18:26:25 +0800

    Added AdminPanel Initial Ui

---
+++ Sourcecodes/AdminDashboard.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html> 
+<html> 
+    <title>Stockmate</title>
+    <link rel = "stylesheet" type ="text/css" href ="AdminDashboard.css">
+    <head> 
+    <body> 
+        <div class = "sidebar">
+               <div class = "avatar">
+                <img src ="/Stockmate/Assets/Avatar.png"  alt ="admin">
+        </div>
+        <nav>
+         <ul> 
+               <li> <a href = "#"> <img src ="/Stockmate/Assets/icon.png" > Dashboard </a></li>
+               <li> <a href ="#"><img src ="/Stockmate/Assets/stockcontrol.png" > Stock Control </a> </li>
+               <li><a href="#"><img src ="/Stockmate/Assets/supplier.png" > Suppliers </a></li>
+                <li><a href="#"><img src ="/Stockmate/Assets/staffs.png" > Staff Management</a></li>
+                <li><a href="#"><img src ="/Stockmate/Assets/logs.png" > Activity Log</a></li>
+
+
+
+
+         </ul>
+
+
+        </nav>
+
+        </div>
+        <div class = main-content> 
+            <h1> Welcome to Stockmate Admin Panel </h1>
+        </div>
+    </body>
+</html>
